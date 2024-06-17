d715833b8df5fe2cc5d9a93f86944e72062f4825
Aviv Laufer <aviv.laufer@rokt.com>
2024-06-17 16:00:06 -0400

    Initial Commit

---
+++ protoconfloader.go
@@ -0,0 +1,327 @@
+package protoconf_loader
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"sync"
+	"sync/atomic"
+
+	pc "github.com/protoconf/protoconf/agent/api/proto/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/fsnotify/fsnotify"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+)
+
+const (
+	AgentDefaultAddress = ":4300"
+)
+
+type Configuration struct {
+	msg              proto.Message
+	serviceName      string
+	logger           *slog.Logger
+	isLoaded         *atomic.Bool
+	isWatchingFile   *atomic.Bool
+	isWatchingAgent  *atomic.Bool
+	configFile       string
+	fsnotifyWatcher  *fsnotify.Watcher
+	mu               sync.RWMutex
+	UnmarshalOptions protojson.UnmarshalOptions
+	quit             chan bool
+	CancelAgent      context.CancelFunc
+	Host             string
+	Port             int
+	agentStub        pc.ProtoconfServiceClient
+
+	onConfigChange func(p proto.Message)
+}
+
+type Option func(*Configuration)
+
+func WithAgentStub(stub pc.ProtoconfServiceClient) Option {
+	return func(c *Configuration) {
+		c.agentStub = stub
+	}
+}
+
+// NewConfiguration creates a new Configuration instance with the given proto.Message,
+// service name, logger, and optional options.
+// It initializes the fsnotify watcher, sets the unmarshal options, and initializes other fields.
+// If any error occurs during the watcher creation, it returns an error.
+func NewConfiguration(p proto.Message, serviceName string, logger *slog.Logger, opts ...Option) (*Configuration, error) {
+	fsnotifyWatcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return nil, err
+	}
+
+	config := &Configuration{
+		msg:             p,
+		serviceName:     serviceName,
+		logger:          logger,
+		isLoaded:        &atomic.Bool{},
+		isWatchingFile:  &atomic.Bool{},
+		isWatchingAgent: &atomic.Bool{},
+		mu:              sync.RWMutex{},
+
+		fsnotifyWatcher: fsnotifyWatcher,
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+		quit:           make(chan bool),
+		onConfigChange: nil,
+	}
+	for _, opt := range opts {
+		opt(config)
+	}
+	return config, nil
+}
+
+// LoadConfig loads the configuration from the specified configPath and configName.
+// If the configuration is already loaded, it returns nil without doing anything.
+// It sets the configFile field to the joined path of configPath and configName,
+// then calls the loadConfig method to actually load the configuration.
+// Finally, it sets the isLoaded field to true and returns nil.
+// If there is an error during loading the configuration, it returns the error.
+func (c *Configuration) LoadConfig(configPath string, configName string) error {
+	if c.isLoaded.Load() {
+		return nil
+	}
+	c.configFile = filepath.Join(configPath, configName)
+	err := c.loadConfig()
+	if err != nil {
+		return err
+	}
+	c.isLoaded.Store(true)
+	return nil
+}
+
+// WatchConfig starts watching the configuration file and the agent for changes.
+// It returns an error if the configuration is not loaded yet.
+// It watches for file changes and agent updates using separate goroutines.
+// The method logs the successful start of watching and returns nil upon successful completion.
+func (c *Configuration) WatchConfig(ctx context.Context) error {
+	if !c.isLoaded.Load() {
+		return errors.New("config is not loaded yet")
+	}
+
+	// Watch config file changes
+	if !c.isWatchingFile.Load() {
+		if err := c.watchFileChanges(); err != nil {
+			return err
+		}
+		c.isWatchingFile.Store(true)
+	}
+
+	// Watch agent changes
+	if !c.isWatchingAgent.Load() {
+		if err := c.listenToChanges(c.serviceName, ctx); err != nil {
+			return err
+		}
+		c.isWatchingAgent.Store(true)
+	}
+
+	c.logger.Info(
+		"Successfully watching config",
+		slog.String("watching_agent_path", c.serviceName),
+		slog.String("watching_file", c.configFile))
+
+	return nil
+}
+
+// watchFileChanges is a method of the Configuration struct that starts watching the configuration file for changes.
+// It adds the configuration file to the fsnotifyWatcher and starts a goroutine to handle file events.
+// When a write event occurs, it calls the loadConfig method to reload the configuration.
+// If there is an error while watching or loading the configuration file, it logs the error.
+// The method returns an error if there is an error adding the file to the fsnotifyWatcher.
+func (c *Configuration) watchFileChanges() error {
+	err := c.fsnotifyWatcher.Add(c.configFile)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-c.fsnotifyWatcher.Events:
+				if !ok {
+					c.logger.Error("error while watching config file", slog.Any("event", event))
+					c.isWatchingFile.Store(false)
+					return
+				}
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					if err := c.loadConfig(); err != nil {
+						c.logger.Error("error while watching and loading config file", slog.Any("error", err))
+					}
+				}
+			case err := <-c.fsnotifyWatcher.Errors:
+				c.logger.Error("error while watching config file", slog.Any("error", err))
+				return
+			}
+		}
+	}()
+
+	return nil
+}
+
+// StopWatching stops watching the configuration file and the agent for changes.
+// It closes the fsnotifyWatcher and cancels the context for the agent connection.
+func (c *Configuration) StopWatching() {
+	if c.isWatchingFile.Load() {
+		c.fsnotifyWatcher.Close()
+	}
+	if c.isWatchingAgent.Load() {
+		c.CancelAgent()
+		c.isWatchingAgent.Store(false)
+	}
+}
+
+// LoadConfig loads the configuration from the specified configPath and configName.
+// If the configuration is already loaded, it returns nil.
+// It sets the configFile field to the joined path of configPath and configName,
+// then calls the loadConfig method to actually load the configuration.
+// Finally, it sets the isLoaded field to true and returns nil.
+// If there is an error during loading the configuration, it returns the error.
+func (c *Configuration) loadConfig() error {
+	var (
+		ErrReadConfigFile  = errors.New("error reading config file")
+		ErrUnmarshalConfig = errors.New("error unmarshaling config")
+	)
+	configReader, err := os.ReadFile(c.configFile)
+	if err != nil {
+		c.logger.Error("error reading config file", slog.Any("error", err))
+		return ErrReadConfigFile
+	}
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	err = c.UnmarshalOptions.Unmarshal(configReader, c.msg)
+	if err != nil {
+		c.logger.Error("error unmarshaling config file", slog.Any("error", err))
+		return ErrUnmarshalConfig
+	}
+
+	if c.onConfigChange != nil {
+		c.onConfigChange(c.msg)
+	}
+	return nil
+}
+
+// listenToChanges is a method of the Configuration struct that establishes a connection to a server using gRPC and subscribes to receive configuration updates.
+// It takes a path string and a context.Context as parameters.
+// It first gets the hostname to use for the connection by calling the getHostname method.
+// Then it dials the server using the obtained address and insecure transport credentials.
+// If there is an error while dialing, it logs the error and returns it.
+// It creates a new ProtoconfServiceClient using the connection.
+// It creates a new context with cancellation capability using the provided context.
+// It subscribes for configuration updates by calling the SubscribeForConfig method of the ProtoconfServiceClient.
+// If there is an error while subscribing, it logs the error and returns it.
+// It starts a goroutine to handle the received configuration updates by calling the handleConfigUpdates method.
+// Finally, it returns nil.
+func (c *Configuration) listenToChanges(path string, ctx context.Context) error {
+	psc := c.agentStub
+	if psc == nil {
+		address := c.getHostname()
+
+		conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			c.logger.Error("Error connecting to server ", slog.String("address", address), slog.Any("error", err))
+			return err
+		}
+
+		psc = pc.NewProtoconfServiceClient(conn)
+	}
+	var agentCtx context.Context
+	agentCtx, c.CancelAgent = context.WithCancel(ctx)
+	stream, err := psc.SubscribeForConfig(agentCtx, &pc.ConfigSubscriptionRequest{Path: path})
+	if err != nil {
+		c.logger.Error("Error subscribing for config", slog.String("path", path), slog.Any("error", err))
+		return err
+	}
+	go c.handleConfigUpdates(stream, path)
+	return nil
+}
+
+// handleConfigUpdates listens for changes to the configuration and invokes the OnConfigChange function.
+func (c *Configuration) handleConfigUpdates(stream pc.ProtoconfService_SubscribeForConfigClient, path string) {
+	for {
+		select {
+		case <-c.quit:
+			c.logger.Info("Stopping listening to changes due to quit signal")
+			return // Exit the goroutine gracefully
+		default:
+			// Read the next update from the stream
+			update, err := stream.Recv()
+			if err == io.EOF {
+				c.logger.Error("Connection closed while streaming config path", slog.String("path", path))
+				return
+			}
+			if err != nil {
+				c.logger.Error("Error unmarshaling config", slog.String("path", path), slog.Any("error", err))
+			}
+
+			// Unmarshal the update into the configuration
+			c.mu.Lock()
+			err = update.GetValue().UnmarshalTo(c.msg)
+			c.mu.Unlock()
+			if err != nil {
+				c.logger.Error("Error while streaming config path", slog.String("path", path), slog.Any("error", err))
+				// Implement appropriate error handling here
+				continue // Continue to the next iteration
+			}
+
+			// Invoke the OnConfigChange function with the updated configuration
+			if c.onConfigChange != nil {
+				c.onConfigChange(c.msg)
+			}
+		}
+	}
+}
+
+// OnConfigChange sets the event handler that is called when a config file changes.
+func (c *Configuration) OnConfigChange(run func(p proto.Message)) {
+	c.onConfigChange = run
+}
+
+// Atomic executes the given function atomically.
+func (c *Configuration) WithLock(f func() error) error {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return f()
+}
+
+// Get returns the result of the given function.
+func Get[T any](c *Configuration, f func() T) T {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return f()
+}
+
+func (c *Configuration) Int32(f func() int32) int32       { return Get(c, f) }
+func (c *Configuration) Int64(f func() int64) int64       { return Get(c, f) }
+func (c *Configuration) UInt32(f func() uint32) uint32    { return Get(c, f) }
+func (c *Configuration) UInt64(f func() uint64) uint64    { return Get(c, f) }
+func (c *Configuration) Float32(f func() float32) float32 { return Get(c, f) }
+func (c *Configuration) Float64(f func() float64) float64 { return Get(c, f) }
+func (c *Configuration) Bool(f func() bool) bool          { return Get(c, f) }
+func (c *Configuration) String(f func() string) string    { return Get(c, f) }
+func (c *Configuration) Any(f func() any) any             { return Get(c, f) }
+
+// getHostname returns the hostname to use for the agent connection.
+// It uses the Host and Port fields if they are set, otherwise it uses the default address.
+func (c *Configuration) getHostname() string {
+	address := fmt.Sprintf("%v:%v", c.Host, c.Port)
+	// Use default if not supplied
+	if address == ":0" {
+		address = AgentDefaultAddress
+	}
+
+	return address
+}
---
+++ protoconfloader_test.go
@@ -0,0 +1,263 @@
+package protoconf_loader
+
+import (
+	"context"
+	"io"
+	"log"
+	"log/slog"
+	"net"
+	"os"
+	"path/filepath"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	protoconfagent "github.com/protoconf/protoconf/agent/api/proto/v1"
+	pb "github.com/protoconf/protoconf/examples/protoconf/src/crawler"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/test/bufconn"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
+)
+
+/**
+ * Test_LoadConfig is a test function that tests the LoadConfig method of the Configuration struct.
+ * It tests the successful loading of a config file, as well as the failure cases of file not found and unmarshal error.
+ *
+ * Args:
+ *   - configPath (string): The path to the config file.
+ *   - configName (string): The name of the config file.
+ *   - serviceName (string): The name of the service.
+ *
+ * Returns:
+ *   - None
+ */
+
+/**
+ * TestConfigFileChanges is a test function that tests the behavior of the Configuration struct when the config file changes.
+ * It initializes the Configuration struct, loads the config, watches the config, and verifies the changes in the config file.
+ *
+ * Args:
+ *   - None
+ *
+ * Returns:
+ *   - None
+ */
+
+func Test_LoadConfig(t *testing.T) {
+	type args struct {
+		configPath  string
+		configName  string
+		serviceName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "test load config success",
+			args: args{
+				configPath:  "./test_data",
+				configName:  "test_config.json",
+				serviceName: "crawler/text_crawler",
+			},
+			wantErr: false,
+		},
+		{
+			name: "test load config fail: file not found",
+			args: args{
+				configPath:  "./test_data",
+				configName:  "not_exist_config.json",
+				serviceName: "crawler/text_crawler",
+			},
+			wantErr: true,
+		},
+		{
+			name: "test load config fail: unmarshal error",
+			args: args{
+				configPath:  "./test_data",
+				configName:  "invalid_config.json",
+				serviceName: "crawler/text_crawler",
+			},
+			wantErr: true,
+		},
+	}
+	config := pb.CrawlerService{}
+	handler := slog.NewJSONHandler(io.Discard, nil)
+	for _, tt := range tests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			c, _ := NewConfiguration(&config, tt.args.serviceName, slog.New(handler))
+			if err := c.LoadConfig(tt.args.configPath, tt.args.configName); (err != nil) != tt.wantErr {
+				t.Errorf("LoadConfig() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+/**
+ * TestConfigFileChanges is a test function that tests the behavior of the Configuration struct when the config file changes.
+ * It initializes the Configuration struct with a protobuf message, a service name, and a logger.
+ * Then, it loads the config file, writes a new config file, watches for changes, waits for the config to be reloaded, and stops watching.
+ * Finally, it asserts that the config has been reloaded correctly.
+ */
+func TestConfigFileChanges(t *testing.T) {
+	// Initialize the Configuration struct
+	testDir := t.TempDir()
+
+	mock := newProtoconfAgentMock(&pb.CrawlerService{LogLevel: 12})
+	c := &pb.CrawlerService{}
+	handler := slog.NewJSONHandler(io.Discard, nil)
+	logger := slog.New(handler)
+	config, _ := NewConfiguration(c, "test_config.json", logger, WithAgentStub(mock.Stub))
+
+	// Load the config
+	err := os.WriteFile(filepath.Join(testDir, "config.json"), []byte("{\"logLevel\":12}"), 0644)
+	assert.NoError(t, err)
+	assert.NotNil(t, config)
+	err = config.LoadConfig(testDir, "config.json")
+
+	assert.NoError(t, err)
+	assert.Equal(t, int32(12), c.LogLevel)
+	// Watch the config
+	var x = atomic.Int32{}
+	x.Store(1)
+	config.OnConfigChange(func(p proto.Message) {
+		x.Store(11)
+	})
+	assert.Equal(t, int32(1), x.Load())
+	err = config.WatchConfig(context.Background())
+	assert.NoError(t, err)
+	err = os.WriteFile(filepath.Join(testDir, "config.json"), []byte("{\"logLevel\":17}"), 0644)
+	assert.NoError(t, err)
+
+	// Wait for the config to be re loaded
+	time.Sleep(1 * time.Second)
+	config.WithLock(func() error {
+		assert.Equal(t, int32(17), c.GetLogLevel())
+		return nil
+	})
+	assert.Equal(t, int32(17), config.Int32(c.GetLogLevel))
+	assert.Equal(t, int32(11), x.Load())
+	config.StopWatching()
+
+}
+
+func TestConfigFileChangesWithAgent(t *testing.T) {
+	c := &pb.CrawlerService{}
+	agent := newProtoconfAgentMock(&pb.CrawlerService{LogLevel: 21})
+	handler := slog.NewJSONHandler(io.Discard, nil)
+	config, err := NewConfiguration(c, "test_config.json", slog.New(handler), WithAgentStub(agent.Stub))
+	assert.NoError(t, err)
+	require.NotNil(t, config)
+
+	testDir := t.TempDir()
+	err = os.WriteFile(filepath.Join(testDir, "config.json"), []byte("{\"logLevel\":21}"), 0644)
+	require.NoError(t, err)
+	err = config.LoadConfig(testDir, "config.json")
+	require.NoError(t, err)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err = config.WatchConfig(ctx)
+	require.NoError(t, err)
+	time.Sleep(1 * time.Second)
+	assert.Equal(t, int32(21), c.LogLevel)
+	agent.SendUpdate(&pb.CrawlerService{LogLevel: 22})
+	time.Sleep(1 * time.Second)
+	assert.Equal(t, int32(22), config.Int32(c.GetLogLevel))
+}
+
+func TestGetHostName(t *testing.T) {
+	tests := []struct {
+		name             string
+		conf             Configuration
+		expectedHostname string
+	}{
+		{
+			name:             "default",
+			conf:             Configuration{},
+			expectedHostname: ":4300",
+		},
+		{
+			name:             "user-provided",
+			conf:             Configuration{Host: "localhost", Port: 4301},
+			expectedHostname: "localhost:4301",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.conf.getHostname(), tt.expectedHostname)
+		})
+	}
+}
+
+type mockResponse struct {
+	wait     time.Duration
+	response *protoconfagent.ConfigUpdate
+}
+
+type protoconfAgentMock struct {
+	protoconfagent.UnimplementedProtoconfServiceServer
+	responses []*mockResponse
+	Chan      chan *protoconfagent.ConfigUpdate
+	Stub      protoconfagent.ProtoconfServiceClient
+	initial   proto.Message
+}
+
+func newProtoconfAgentMock(initial proto.Message, responses ...*mockResponse) *protoconfAgentMock {
+	buffer := 1024 * 1024
+	lis := bufconn.Listen(buffer)
+	ctx := context.Background()
+	rpcServer := grpc.NewServer()
+	srv := &protoconfAgentMock{responses: responses, Chan: make(chan *protoconfagent.ConfigUpdate), initial: initial}
+	protoconfagent.RegisterProtoconfServiceServer(rpcServer, srv)
+	go func() {
+		context.AfterFunc(ctx, func() {
+			rpcServer.GracefulStop()
+		})
+		if err := rpcServer.Serve(lis); err != nil {
+			log.Printf("error serving server: %v", err)
+		}
+	}()
+
+	conn, err := grpc.DialContext(ctx, "",
+		grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) {
+			return lis.Dial()
+		}), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Printf("error connecting to server: %v", err)
+	}
+	srv.Stub = protoconfagent.NewProtoconfServiceClient(conn)
+	return srv
+}
+
+func (p protoconfAgentMock) SubscribeForConfig(request *protoconfagent.ConfigSubscriptionRequest, srv protoconfagent.ProtoconfService_SubscribeForConfigServer) error {
+	go func() {
+		for _, r := range p.responses {
+			time.Sleep(r.wait)
+			srv.Send(r.response)
+		}
+	}()
+	for {
+		select {
+		case <-srv.Context().Done():
+			return srv.Context().Err()
+		case update := <-p.Chan:
+			srv.Send(update)
+		}
+	}
+}
+
+func (p protoconfAgentMock) SendUpdate(msg proto.Message) {
+	v, _ := anypb.New(msg)
+	update := &protoconfagent.ConfigUpdate{
+		Value: v,
+	}
+	go func() {
+		p.Chan <- update
+	}()
+}
